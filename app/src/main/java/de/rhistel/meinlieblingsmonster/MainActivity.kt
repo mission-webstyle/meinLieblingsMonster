4685f7dbbb9367a56e6fd859861a934d4fcf1491
rhistel <kontakt@mission-webstyle.de>
2019-12-12 13:17:45 +0100

    Innit commit

---
+++ app/src/main/java/de/rhistel/meinlieblingsmonster/MainActivity.kt
@@ -0,0 +1,64 @@
+package de.rhistel.meinlieblingsmonster
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.main_activity_content_layout.*
+
+import kotlinx.android.synthetic.main.main_activity_layout.*
+
+/**
+ * Einstiegspunkt in die App
+ */
+class MainActivity : AppCompatActivity() {
+
+    //region 1 Lebenszyklus
+
+    /**
+     * Startet als erstes nach dem Konstruktor
+     * Setzt das Layout und generiert alle direkt
+     * benoetigten Widgets. Setzt Listener
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //1. Layout setzen
+        this.setContentView(R.layout.main_activity_layout)
+
+        //2. Setzen der Toolbar
+        setSupportActionBar(mainToolbar)
+
+        //3. Fab Button mit Listener belegen
+        this.fabSaveUserName.setOnClickListener() {
+            val strUserName = txtInputUserName.text.toString()
+
+            if(strUserName.isNotEmpty()) {
+                val strUserNameMessage = getString(R.string.strUserNameMessage) + " " + strUserName
+
+                Snackbar.make(it, strUserNameMessage, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            }
+        }
+    }
+
+    //endregion
+
+    //region 2. MenuHandling
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+//endregion
+}
