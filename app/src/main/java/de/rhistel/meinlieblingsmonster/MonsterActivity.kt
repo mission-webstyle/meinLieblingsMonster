df3cb25a60d153c77c5a0378aa7e4ba3447ae227
rhistel <kontakt@mission-webstyle.de>
2019-12-13 14:59:16 +0100

    Anzeigen von Bilder aus dem Drawable ordner mit meheren Bildern im Drawable ordner

--- app/src/main/java/de/rhistel/meinlieblingsmonster/MonsterActivity.kt
+++ app/src/main/java/de/rhistel/meinlieblingsmonster/MonsterActivity.kt
@@ -39,7 +39,7 @@ class MonsterActivity : AppCompatActivity() {
      */
     fun loadImage(v: View) {
         when (v.id) {
-            R.id.btnShowMonsterOne -> showImageFromDrawableResourceInImageView(R.drawable.monster02)
+            R.id.btnShowMonsterOne -> showImageFromDrawableResourceInImageView(R.drawable.monster01)
             R.id.btnShowMonsterTwo -> showImageFromAssetsInImageView(FILE_NAME_MONSTER_TWO)
             R.id.btnShowMonsterThree -> showImageFromAssetsInImageView(FILE_NAME_MONSTER_THREE)
         }
